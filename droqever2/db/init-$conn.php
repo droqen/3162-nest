c44f38f31b06ded3a94c94aefa260282e5b8eab9
Alexander Martin <hi@droqen.com>
2024-06-01 23:49:17 -0400

    a working htaccess and requires with __DIR__

---
+++ droqever2/db/init-$conn.php
@@ -0,0 +1,10 @@
+<?php
+	echo "<p>require - initconn</p>";
+	require_once __DIR__ . '/.dbcredentials.php';
+	$dsn = "mysql:host=$dbhost;dbname=$dbname";
+	$options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
+	try {
+		$conn = new PDO($dsn, $dbuser, $dbpass, $options);
+	} catch (PDOException $e) {
+		die("<b>Problem!</b> PDO connection failed: " . $e->getMessage() . "<br/>");
+	}
