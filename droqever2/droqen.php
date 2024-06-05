32a331a73c6bd155bf19ad43787ad8d036dfebdc
Alexander Martin <hi@droqen.com>
2024-06-05 00:06:51 -0400

    removed some old comment

--- droqever2/droqen.php
+++ droqever2/droqen.php
@@ -6,9 +6,4 @@ if (isset($_SESSION['username']) && $_SESSION['username'] == 'droqen') {
 	require_once __DIR__ . '/pages/admin-page.php';
 } else {
 	require_once __DIR__ . '/pages/login-page.html';
-	echo $_GET['droqen'];
-	echo '<br/>';
-	echo $_GET['droqen_proof'];
-	echo '<br/>';
-	// && $_GET['droqen_proof']=="i%27m+droqen%21"
 }
