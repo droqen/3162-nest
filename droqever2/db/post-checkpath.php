440a08bf02b4be421b1076e056df326a42103445
Alexander Martin <hi@droqen.com>
2024-12-08 02:58:05 -0500

    new db call for checking pathway

---
+++ droqever2/db/post-checkpath.php
@@ -0,0 +1,32 @@
+<?php
+	require_once __DIR__ . '/init-$conn.php';
+
+	if (!isset($_POST['from_postid'])) { die("Missing param 7203"); }
+	if (!isset($_POST['exitname'])) { die("Missing param 3817"); }
+
+	$from_postid = $_POST['from_postid'];
+	$exitname = $_POST['exitname'];
+
+	$stmt = $conn->prepare("SELECT to_postid, entername FROM pathways WHERE from_postid=? AND exitname=? LIMIT 1");
+	$stmt->execute([$from_postid, $exitname]);
+	$row = $stmt->fetch();
+	if ($row) {
+		$to_postid = $row[0];
+		$entername = $row[1];
+		if (true) { // check posts to get games table name
+			$stmt = $conn->prepare("SELECT zipname, zipsize FROM games_43 WHERE postid=? LIMIT 1");
+			$stmt->execute([$to_postid]);
+			$row = $stmt->fetch();
+			if ($row) {
+				$zipname = $row[0];
+				$zipsize = $row[1];
+				die("OK!;$zipname;$zipsize");
+			} else {
+				die("Problem! to_postid value $to_postid not found on table games_43");
+			}
+		} else {
+			// pass
+		}
+	} else {
+		die("Problem! from_postid . exitname pair '$from_postid . $exitname' not found on table 'pathways'");
+	}
